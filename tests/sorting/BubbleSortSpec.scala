031c7f932e0c26b6d078950bbd65cd29aec065a1
ascensio <daedalon@gmx.de>
2015-02-21 07:24:23 +0100

    BubbleSort

--- tests/sorting/BubbleSortSpec.scala
+++ tests/sorting/BubbleSortSpec.scala
@@ -3,6 +3,8 @@ package sorting
 import org.salgo.sorting.{BubbleSort, GeneralSortingAlgorithm}
 
 class BubbleSortSpec extends GenericSortSpec {
+  test("Sort long list 2") { BubbleSort.sort(List(6, 1, 0, 3, 3, 5, 7, 2, 1, 6, 5, 4 ,3 ,1, 2, 8, 8, 9 ,7)) shouldEqual List(0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9) }
+
   override def getSortingAlgorithm: GeneralSortingAlgorithm = BubbleSort
 }
 
