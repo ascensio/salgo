a2f061abbcba314ddfb6f8b228960d98699d2066
ascensio <daedalon@gmx.de>
2015-02-21 07:24:46 +0100

    BubbleSort

--- src/org.salgo/sorting/BubbleSort.scala
+++ src/org.salgo/sorting/BubbleSort.scala
@@ -1,8 +1,9 @@
 package org.salgo.sorting
 
+import scala.annotation.tailrec
 import scala.reflect.ClassTag
 
-object BubbleSort extends GeneralSortingAlgorithm {
+object BubbleSort extends GeneralSortingAlgorithm with GeneralFunctionalSortingAlgorithm {
   def sort[T <: Any : ClassTag](seq: Array[T])(implicit ev: T => Ordered[T]) : Unit = {
     var swapped = true
     var j = 0
@@ -16,5 +17,18 @@ object BubbleSort extends GeneralSortingAlgorithm {
         }
       }
     }
+  }
+
+  override def sort[T <: Any : ClassTag](seq: Seq[T])(implicit ev: (T) => Ordered[T]): Seq[T] = {
+    this.sort(seq, Nil, Nil, continue = false)
+  }
+
+  @tailrec
+  private def sort[T <: Any : ClassTag](seq: Seq[T], acc: Seq[T], finalAcc: Seq[T], continue: Boolean)(implicit ev: T => Ordered[T]) : Seq[T] = seq match {
+    case Nil => finalAcc
+    case (h :: Nil) if !continue => acc ++ (h +: finalAcc)
+    case (h :: Nil) if continue => this.sort(acc, Nil, h +: finalAcc, continue = false)
+    case (h1 :: h2 :: t) if h1 > h2 =>  this.sort(h1 :: t, acc :+ h2, finalAcc, continue = true)
+    case (h1 :: h2 :: t) => this.sort(h2 :: t, acc :+ h1, finalAcc, continue)
   }
 }
